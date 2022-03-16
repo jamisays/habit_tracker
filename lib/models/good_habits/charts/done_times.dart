1ae867b2d11f76c5a8c49b65ad8fb44fcf5c5bd4
MD Kawsar Ahmed Jami <jami@infoinlet.com>
2022-03-16 12:15:44 +0600

    before new project

---
+++ lib/models/good_habits/charts/done_times.dart
@@ -0,0 +1,45 @@
+import 'package:habit_tracker/models/events/eventSource.dart';
+import 'package:habit_tracker/models/good_habits/goodHabit.dart';
+import 'package:habit_tracker/utils.dart';
+import 'package:hive/hive.dart';
+
+class DoneTimes {
+  DoneTimes(this.date, this.doneTimes);
+  DateTime date;
+  int doneTimes;
+}
+
+List<DoneTimes> getChartData(GoodHabit selectedHabit) {
+  List<DoneTimes> chartData = [];
+  var currentDay = DateTime.now();
+  // var eventsData;
+  // Hive.openBox('event_source').then((value) {
+  var value = Hive.box<EventSource>('event_source');
+
+  if (value.values.first.eventSource != null) {
+    for (var i = currentDay;
+        DateTime.now().difference(i).inDays < 7;
+        i = i.subtract(Duration(days: 1))) {
+      var x = 0;
+      kEvents = value.values.first.eventSource!;
+      var dayEvents = kEvents[DateTime.utc(i.year, i.month, i.day)];
+      if (dayEvents != null) {
+        dayEvents.forEach((element) {
+          if (element.title == selectedHabit.title) {
+            x = x + element.doneTimes;
+          }
+        });
+      }
+      chartData.add(
+        DoneTimes(
+          DateTime.utc(i.year, i.month, i.day),
+          x,
+        ),
+      );
+    }
+  }
+  // ignore: unnecessary_null_comparison
+
+  return chartData;
+  // });
+}
